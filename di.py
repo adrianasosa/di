7b9a9f6d150cf097af1025200d3d94f5d60098bf
adrianasosa <52220952+adrianasosa@users.noreply.github.com>
2019-06-25 15:51:46 -0600

    Add files via upload

---
+++ di.py
@@ -0,0 +1,203 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n= input ("Ingrese el valor de n: ")
+Ingrese el valor de n: 6
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 1
+>>> x= x
+>>> i=n
+>>> for i in range ( x,i):
+	def potencia (x,i):
+		return x**i
+	potencia (x,i)
+
+	
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    for i in range ( x,i):
+TypeError: 'str' object cannot be interpreted as an integer
+>>> n = input ("Ingrese el valor de n: ")
+Ingrese el valor de n: 6
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 1
+>>> x= int (x)
+>>> i = int (n)
+>>> for i in range (x,i):
+	def potencia (x,i):
+		return x**i
+	potencia (x,i)
+
+	
+1
+1
+1
+1
+1
+>>> n = input ("Ingrese el valor de m: ")
+Ingrese el valor de m: 9
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 5
+>>> n= int (n)
+>>> x= int (x)
+>>> for i in range (x, n):
+	def potencia (x,n):
+		return x**n
+	potencia (x,n)
+
+	
+1953125
+1953125
+1953125
+1953125
+>>> n= input ("Ingrese el valor de n:")
+Ingrese el valor de n:20
+>>> x = int (n)
+>>> for i in range (1,20):
+	def potencia (x,i)
+	
+SyntaxError: invalid syntax
+>>> n = input ("Ingrese el valor de n: ")
+Ingrese el valor de n: 20
+>>> n= int (n)
+>>> x= int (1)
+>>> for i in range (1,20):
+	def potencia (x,i):
+		      return x**i
+
+		
+>>> 
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x= int (x)
+>>> for i in range (1, x):
+	def potencia (x,i):
+		return x**i
+	potencia (x,i)
+
+	
+20
+400
+8000
+160000
+3200000
+64000000
+1280000000
+25600000000
+512000000000
+10240000000000
+204800000000000
+4096000000000000
+81920000000000000
+1638400000000000000
+32768000000000000000
+655360000000000000000
+13107200000000000000000
+262144000000000000000000
+5242880000000000000000000
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x= int (x)
+>>> fact = 1
+>>> for i in range (1,x):
+	fact = fact * i
+	print (fact)
+
+	
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+39916800
+479001600
+6227020800
+87178291200
+1307674368000
+20922789888000
+355687428096000
+6402373705728000
+121645100408832000
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x= int (x)
+>>> for i in range (1,x):
+	def potencia (x,i):
+		return x**i
+	potencia (j,i)
+	fact = 1
+	for i in range (1,x):
+		fact = fact * 1
+		print (fact)
+		div= potencia / fact
+		print (div)
+
+		
+Traceback (most recent call last):
+  File "<pyshell#65>", line 4, in <module>
+    potencia (j,i)
+NameError: name 'j' is not defined
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x = int (x)
+>>> for i in range (1,x):
+	def potencia (x,i):
+		return x**i
+	potencia = (j,i)
+	fact = 1
+	for i in range (1,x):
+		fact = fact *i
+		print (fact)
+		div = potencia /fact
+		print (div)
+
+		
+Traceback (most recent call last):
+  File "<pyshell#83>", line 4, in <module>
+    potencia = (j,i)
+NameError: name 'j' is not defined
+>>> x= input ("Ingrese el valor de x: )
+	  
+SyntaxError: EOL while scanning string literal
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x= int (x)
+>>> for i in range (1,x):
+	def potencia (x,i):
+		return x**i
+	potencia = (x,i)
+	fact = 1
+	for i in range (1,x):
+		fact = fact * i
+		div = potencia / fact
+		print (div)
+
+		
+Traceback (most recent call last):
+  File "<pyshell#96>", line 8, in <module>
+    div = potencia / fact
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x= int (x)
+>>> po= for i in range (1,x):
+	
+SyntaxError: invalid syntax
+>>> potencia/ fact
+Traceback (most recent call last):
+  File "<pyshell#100>", line 1, in <module>
+    potencia/ fact
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+>>> x= input ("Ingrese el valor de x: ")
+Ingrese el valor de x: 20
+>>> x = int (x)
+>>> 
